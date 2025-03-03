788e8b14dd934e8f3b67cdeaf682abee5f0d5ae5
Subhash Yadav <147519022+subhash-yadav1@users.noreply.github.com>
2025-03-03 19:44:17 +0545

    new page added to the site

--- components/navbar/nav.js
+++ components/navbar/nav.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("components/navbar/nav.html")
+  fetch("/components/navbar/nav.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("nav").innerHTML = data;
